0fadb81fb32f47d8383aa7c1c717dd38e17c17f7
jakukow <jakubkowolik00@gmail.com>
2024-10-29 23:39:13 +0100

    Plan layout started

--- app/(dashboard)/plans/page.tsx
+++ app/(dashboard)/plans/page.tsx
@@ -1,6 +1,38 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 const PlansPage = () => {
   return (
-    <div className="mt-11  mr-11 flex flex-col w-full h-[full] gap-y-5 shadow bg-white rounded-xl "></div>
+    <div className="mt-11 mx-5 flex justify-center items-center w-full h-[full]   ">
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className=" w-full max-w-sm md:max-w-3xl lg:max-w-5xl"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex  items-center justify-center ">
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 };
 
