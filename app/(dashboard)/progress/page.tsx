e238178b894cb7c6b1418e885131969ebe4feb5d
jakukow <jakubkowolik00@gmail.com>
2024-11-08 19:20:29 +0100

    Progress - layout started

--- app/(dashboard)/progress/page.tsx
+++ app/(dashboard)/progress/page.tsx
@@ -1,7 +1,14 @@
 const ProgressPage = () => {
   return (
-    <div className="mt-11 mx-5 items-center justify-center flex flex-col w-full h-full shadow bg-white rounded-xl overflow-hidden">
-      haha
+    <div className="mt-11 mx-5 items-center justify-center flex w-full h-full max-h-[750px] shadow bg-white rounded-xl overflow-hidden">
+      <div className="hidden md:w-1/3 h-full text-white font-bold md:flex prim">
+        <div className="flex flex-col m-10 justify-around"></div>
+      </div>
+      <div className="w-full md:w-2/3 h-full flex flex-col bg-white">
+        <div className="flex m-4 h-full">
+          <div className="bg-slate-200 rounded-xl w-full p-4 overflow-y-scroll no-scrollbar h-[720px]"></div>
+        </div>
+      </div>
     </div>
   );
 };
