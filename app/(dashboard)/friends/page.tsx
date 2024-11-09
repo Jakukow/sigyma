8b0a5114ff7da943f7ed88ba67909ff0a9592061
jakukow <jakubkowolik00@gmail.com>
2024-11-10 00:44:17 +0100

    Friends layout - started

--- app/(dashboard)/friends/page.tsx
+++ app/(dashboard)/friends/page.tsx
@@ -1,7 +1,22 @@
 const FriendsPage = () => {
   return (
-    <div className="mt-11 mx-5 items-center justify-center flex flex-col w-full h-full shadow bg-white rounded-xl overflow-hidden">
-      haha
+    <div className="mt-11 mx-5  flex  w-full h-full shadow bg-white rounded-xl overflow-hidden">
+      <div className="flex w-1/3 m-4">
+        <div className="flex  w-full text-prim font-bold mx-2 tracking-widest">
+          Friends List
+        </div>
+      </div>
+      <div className=" w-1/3 m-4  flex h-full">
+        <div className="flex  w-full text-prim font-bold mx-2 tracking-widest">
+          People You May Know
+        </div>
+      </div>
+
+      <div className=" w-1/3 m-4  flex h-full ">
+        <div className="flex  w-full text-prim font-bold mx-2 tracking-widest">
+          Invites
+        </div>
+      </div>
     </div>
   );
 };
