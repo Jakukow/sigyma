d26b66a2e41481f580194373958e74673f0980fe
jakukow <jakubkowolik00@gmail.com>
2024-11-12 18:46:51 +0100

    Hono.Js, Drizzle and NeonDB added

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,13 @@
+//import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import authors from "./authors";
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.route("/authors", authors);
+
+export const GET = handle(app);
+export const POST = handle(app);
